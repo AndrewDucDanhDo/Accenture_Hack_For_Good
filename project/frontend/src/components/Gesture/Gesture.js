7d98eed47bac425c72d1775fbc44803ad1b54b87
Tony Cui <tonycui02@gmail.com>
2021-01-17 11:47:11 +1300

    gesture initial

---
+++ project/frontend/src/components/Gesture/Gesture.js
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from "react";
+import * as tf from "@tensorflow/tfjs";
+import * as tmImage from "@teachablemachine/image";
+
+export default function Gesture() {
+  // Classifier Variable
+  let classifier;
+  // Model URL
+  let imageModelURL =
+    "https://teachablemachine.withgoogle.com/models/qRxTkH0Lt/";
+
+  // Video
+  let video;
+  let flippedVideo;
+  // To store the classification
+  let label = "";
+
+  // Load the model first
+  function preload() {
+    classifier = ml5.imageClassifier(imageModelURL + "model.json");
+  }
+
+  function setup() {
+    createCanvas(320, 260);
+    // Create the video
+    video = createCapture(VIDEO);
+    video.size(320, 240);
+    video.hide();
+
+    flippedVideo = ml5.flipImage(video);
+    // Start classifying
+    classifyVideo();
+  }
+
+  function draw() {
+    background(0);
+    // Draw the video
+    image(flippedVideo, 0, 0);
+
+    // Draw the label
+    fill(255);
+    textSize(16);
+    textAlign(CENTER);
+    text(label, width / 2, height - 4);
+  }
+
+  // Get a prediction for the current video frame
+  function classifyVideo() {
+    flippedVideo = ml5.flipImage(video);
+    classifier.classify(flippedVideo, gotResult);
+    flippedVideo.remove();
+  }
+
+  // When we get a result
+  function gotResult(error, results) {
+    // If there is an error
+    if (error) {
+      console.error(error);
+      return;
+    }
+    // The results are in an array ordered by confidence.
+    // console.log(results[0]);
+    label = results[0].label;
+    // Classifiy again!
+    classifyVideo();
+  }
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  return (
+    <div>
+      <div className="title">Wave with your RIGHT hand</div>
+      <button type="button" onclick="init()">
+        Start
+      </button>
+      <div id="webcam-container"></div>
+      <div id="label-container"></div>
+    </div>
+  );
+}
